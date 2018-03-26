e4b2701680d1baa7e47f13821efbb7b63e8dd455
William Spelman <williamchendev@gmail.com>
2018-03-26 10:00:12 -0400

    Update 0.16
    Added sound and fixes

--- thevote/Assets/Scripts/Debug/DebugEvent.cs
+++ thevote/Assets/Scripts/Debug/DebugEvent.cs
@@ -23,6 +23,8 @@ public class DebugEvent : MonoBehaviour {
         file.addText("I can't believe this is happening", Vector2.zero, "player");
         file.addEnd();
         */
+
+        /*
         file.addSkip(1, 22);
         file.addKey(0);
         file.addText("..auuuuggggggghhh...", Vector2.zero, "will");
@@ -60,6 +62,7 @@ public class DebugEvent : MonoBehaviour {
         file.addText("We have about 3 hours to get 500 signatures", Vector2.zero, "ann");
         file.addText("I think we can do it <3", Vector2.zero, "player");
         file.addEnd();
+        */
 
         /*
         file.addKey(4);
@@ -73,7 +76,71 @@ public class DebugEvent : MonoBehaviour {
         file.addText("okay Okay I Got it", Vector2.zero, "ann");
         file.addEnd();
         */
-        em.saveFile("TestText", file);
+
+        
+        file.addSkip(0, 20);
+        file.addKey(0);
+        file.addText("Omg you weren't kidding", Vector2.zero, "player");
+        file.addText("I told you so", Vector2.zero, "Will");
+        file.addText("Your favorite diner opened up in Naperville", Vector2.zero, "Will");
+        file.addText("I can't beleive it's so empty", Vector2.zero, "player");
+        file.addChoice("Will", "What are you going to order?", "Waffle Fries", 1, "Cheesy Waffle Fries", 1);
+        file.addText("I thought you said we were getting waffles", Vector2.zero, "Will");
+        file.addText("Waffle fries are a type of waffle!", Vector2.zero, "player");
+        file.addText("...", Vector2.zero, "Will");
+        file.addText("What?", Vector2.zero, "player");
+        file.addText("I missed you so much", Vector2.zero, "Will");
+        file.addChoice("Will", "What made you come back from LA?", "Waffle Fries", 1, "Cheesy Waffle Fries", 1);
+        file.addText("...", Vector2.zero, "Will");
+        file.addText("You're such a dumpster fire", Vector2.zero, "Will");
+        file.addText("Just grab us some food so we don't starve", Vector2.zero, "Will");
+        file.addText("Anything you want in particular?", Vector2.zero, "player");
+        file.addText("Some plastic spoons to gouge my eyes out", Vector2.zero, "Will");
+        file.addText("Sure thing <3", Vector2.zero, "player");
+        file.addSkip(0, 30);
+
+        file.addSkip(1, 4);
+        file.addText("I'm starvinggggggggg", Vector2.zero, "Will");
+        file.addText("hurry up pleeeeaseeee", Vector2.zero, "Will");
+        file.addSkip(0, 26);
+
+        file.addText("So guess what I have for you", Vector2.zero, "Will");
+        file.addText("Wait! Last time you gave me a surprise I had a panic attack", Vector2.zero, "player");
+        file.addChoice("Will", "Since when did my gift giving hurt anyone?", "Last Febuary", 1, "May two years ago", 1, "The Christmas Apocalypse", 1);
+        file.addText("Old people get heart attacks all the time", Vector2.zero, "Will");
+        file.addText("He died of shock", Vector2.zero, "player");
+        file.addText("WITH A SMILE ON HIS FACE", Vector2.zero, "Will");
+        file.addText("You can't argue with results", Vector2.zero, "Will");
+
+        file.addText("But no, this is something entirely different!", Vector2.zero, "Will");
+        file.addText("It's a job!", Vector2.zero, "Will");
+        file.addText("...", Vector2.zero, "player");
+        file.addText("How come you're not that excited?", Vector2.zero, "Will");
+        file.addText("In today's economy you just practically won the lottery!", Vector2.zero, "Will");
+        file.addText("~yayyy~", Vector2.zero, "player");
+        file.addChoice("Will", "You're so lame", "Do I at least get paid?", 1, "What do I have to do?", 1);
+        file.addText("Glad you asked!", Vector2.zero, "Will");
+        file.addText("We're having an upcoming election for mayor!", Vector2.zero, "Will");
+        file.addText("Isn't the mayor alive?", Vector2.zero, "player");
+        file.addText("Nope, not since yesterday at least...", Vector2.zero, "Will");
+        file.addText("Omg!", Vector2.zero, "player");
+        file.addText("He had one of those heart attacks", Vector2.zero, "Will");
+        file.addText("really common among old people", Vector2.zero, "Will");
+        file.addText("What about his advisors? Or his board?", Vector2.zero, "player");
+        file.addText("None of them are running.", Vector2.zero, "Will");
+        file.addText("In fact all of them declined to enter this year's campaign", Vector2.zero, "Will");
+        file.addText("Which brings us to our main ", Vector2.zero, "Will");
+
+        file.addEnd();
+
+        /*
+        file.addText("Waffles?", Vector2.zero, "Ellie");
+        file.addText("Yes", Vector2.zero, "player");
+        file.addKey(1);
+        file.addEnd();
+        */
+
+        em.saveFile("WillDinerA", file);
     }
 
 }
