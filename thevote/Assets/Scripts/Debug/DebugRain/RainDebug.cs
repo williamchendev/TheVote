24377f926f6559ce6b08985fa53760d6dc6d67a3
William Spelman <williamchendev@gmail.com>
2018-04-08 00:24:35 -0400

    Update 0.23
    Added the rest of the art assets for the main rooms and arranged the scenes for the main rooms

---
+++ thevote/Assets/Scripts/Debug/DebugRain/RainDebug.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDebug : MonoBehaviour {
+
+    [SerializeField] private float y_offset = 0f;
+
+	void Update () {
+        transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y + y_offset, 2.5f);
+	}
+
+}
---
+++ thevote/Assets/Scripts/Debug/DebugRain/SplashDebug.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashDebug : MonoBehaviour {
+
+    [SerializeField] private float y_offset = 0f;
+
+	private int counter;
+
+    void Start() {
+        counter = 0;
+    }
+
+    void Update () {
+        counter--;
+        if (counter <= 0){
+		    transform.position = new Vector3(Camera.main.transform.position.x + Random.Range(-7.5f, 7.5f), Camera.main.transform.position.y + Random.Range(-2f, -4f) + y_offset, transform.position.z);
+            counter = Random.Range(5, 7);
+        }
+	}
+
+}
